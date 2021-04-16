805a159f457f2405eb84cae5a4eb7bf07e27e561
Greg <russki_canuck@hotmail.com>
2021-04-16 13:23:53 -0400

    update

---
+++ Data/UserDataService.cs
@@ -0,0 +1,13 @@
+namespace BlazorServerExample.Data
+{
+
+    public class UserDataService
+    {
+
+        public string Name { get; set; }
+
+        public uint ComponentCount { get; set; } = 0;
+
+    }
+
+}
